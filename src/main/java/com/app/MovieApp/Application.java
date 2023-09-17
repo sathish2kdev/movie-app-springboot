d432bb282e651779f0979c43e7afb173866a0536
Sathish S <105538627+sathish2kdev@users.noreply.github.com>
2023-09-17 12:05:02 +0530

    Update Application.java

--- src/main/java/com/app/MovieApp/Application.java
+++ src/main/java/com/app/MovieApp/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+//Sathish
+
 @SpringBootApplication
 public class Application {
 
